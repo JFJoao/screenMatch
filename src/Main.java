ff4ac796940c21bff59d10795842a7744d3e1632
JFJoao <jfjoaojf@hotmail.com>
2023-12-07 22:42:41 -0300

    update with abstraction and encapsulation

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import br.com.jfjoao.screemMatch.modelos.Filme;
+
+public class Main {
+    public static void main(String[] args) {
+        // Cadastro de novo filme
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("300");
+        meuFilme.setAnoDeLancamento(2009);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setSinopse("Uma obra de arte!") ;
+        meuFilme.setIncluidoNoPlano(true);
+        // Bloco de avaliações
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(10);
+        meuFilme.avalia(9);
+        meuFilme.avalia(8.8);
+        // Informações do filme
+        meuFilme.exibeFichaTecnica();
+        System.out.println("Nota: " + meuFilme.obterMediaAvaliacoes());
+        System.out.println("Votos: " + meuFilme.getTotalDeAvaliacoes());
+    }
+}
---
+++ src/br/com/jfjoao/screemMatch/modelos/Filme.java
@@ -0,0 +1,59 @@
+package br.com.jfjoao.screemMatch.modelos;
+
+public class Filme {
+    // Atributos
+    private String nome;
+    private String sinopse;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double avaliacao;
+    private int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int duracaoEmMinutos;
+
+    // Getters
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    // Setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setSinopse(String sinopse){
+        this.sinopse = sinopse;
+    }
+    public void setAnoDeLancamento(int anoDeLancamento){
+        this.anoDeLancamento = anoDeLancamento;
+    }
+    public void setIncluidoNoPlano(Boolean incluidoNoPlano){
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+    private void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    // Métodos
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento" + anoDeLancamento);
+        System.out.println("Sinopse: " + sinopse);
+        System.out.println("Nota: " + avaliacao );
+        System.out.println("Total avaliações: " + totalDeAvaliacoes);
+        System.out.println("Duração: " + duracaoEmMinutos + " minutos.");
+        System.out.println("Premium: " + incluidoNoPlano);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double obterMediaAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+        }
+    }
+
