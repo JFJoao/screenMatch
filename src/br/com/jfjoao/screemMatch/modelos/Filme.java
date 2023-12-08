edfa36d78deb81d4cade66eabe0eee26f88b41ae
JFJoao <jfjoaojf@hotmail.com>
2023-12-07 23:34:55 -0300

    update with heritage

--- src/br/com/jfjoao/screemMatch/modelos/Filme.java
+++ src/br/com/jfjoao/screemMatch/modelos/Filme.java
@@ -1,55 +1,18 @@
 package br.com.jfjoao.screemMatch.modelos;
 
-public class Filme {
-    // Atributos
-    private String nome;
-    private String sinopse;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double avaliacao;
-    private int totalDeAvaliacoes;
-    private double somaDasAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo{
 
-    // Getters
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    // Atributo
+    private String diretor;
+
+    // Setter
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    // Setters
-    public void setNome(String nome){
-        this.nome = nome;
+    // Getter
+    public String getDiretor() {
+        return diretor;
     }
-    public void setSinopse(String sinopse){
-        this.sinopse = sinopse;
-    }
-    public void setAnoDeLancamento(int anoDeLancamento){
-        this.anoDeLancamento = anoDeLancamento;
-    }
-    public void setIncluidoNoPlano(Boolean incluidoNoPlano){
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-    private void setAvaliacao(double avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-    // Métodos
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lancamento" + anoDeLancamento);
-        System.out.println("Sinopse: " + sinopse);
-        System.out.println("Nota: " + avaliacao );
-        System.out.println("Total avaliações: " + totalDeAvaliacoes);
-        System.out.println("Duração: " + duracaoEmMinutos + " minutos.");
-        System.out.println("Premium: " + incluidoNoPlano);
-    }
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-    public double obterMediaAvaliacoes(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-        }
+
     }
---
+++ src/br/com/jfjoao/screemMatch/modelos/Serie.java
@@ -0,0 +1,40 @@
+package br.com.jfjoao.screemMatch.modelos;
+
+public class Serie extends Titulo{
+
+    //Atributos
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private boolean ativa;
+    private int minutosPorEpisodio;
+
+    // Setters
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    // Getters
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+    public boolean isAtiva() {
+        return ativa;
+    }
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+}
